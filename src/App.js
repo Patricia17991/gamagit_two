1555e4a8ad5e61ae6194fefbbdf39f669105dda3
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2021-08-03 13:40:21 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Routes from './routes';
+import React, {useState} from 'react';
+import './styled.css';
+
+
+function App(props) {
+  return (
+    <>
+    <Routes/>
+    </>
+  );
+}
+
+export default App;
